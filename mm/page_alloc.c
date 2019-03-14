382601a6bb9d57db8d8026723a2cf25211b801e8
Tyler Nijmeh <tylernij@gmail.com>
2019-03-13 20:21:57 -0700

    qcacld-3.0: Do not allow any wakelocks to be held
    These contribute to a great amount of idle drain.
    
    Tests: 30 minutes of playing Spotify with the screen off, unplugged.
    
    Change-Id: Ie6add08ce0dcc261796a6c2a374cd2fd7c71815c
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -341,7 +341,7 @@ int watermark_boost_factor __read_mostly;
 #else
 int watermark_boost_factor __read_mostly = 0;
 #endif
-int watermark_scale_factor = 10;
+int watermark_scale_factor = 125;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
