dd32dddb3a165af882d28863d1650202e352bedd
Alessio Balsini <balsini@google.com>
2021-01-25 17:03:31 +0000

    Update

--- fs/fuse/passthrough.c
+++ fs/fuse/passthrough.c
@@ -14,18 +14,6 @@ struct fuse_aio_req {
 	struct kiocb *iocb_fuse;
 };
 
-static inline void kiocb_clone(struct kiocb *kiocb, struct kiocb *kiocb_src,
-			       struct file *filp)
-{
-	*kiocb = (struct kiocb){
-		.ki_filp = filp,
-		.ki_flags = kiocb_src->ki_flags,
-		.ki_hint = kiocb_src->ki_hint,
-		.ki_ioprio = kiocb_src->ki_ioprio,
-		.ki_pos = kiocb_src->ki_pos,
-	};
-}
-
 static void fuse_file_accessed(struct file *dst_file, struct file *src_file)
 {
 	struct inode *dst_inode;
@@ -81,6 +69,18 @@ static void fuse_aio_rw_complete(struct kiocb *iocb, long res, long res2)
 
 	fuse_aio_cleanup_handler(aio_req);
 	iocb_fuse->ki_complete(iocb_fuse, res, res2);
+}
+
+static inline void kiocb_clone(struct kiocb *kiocb, struct kiocb *kiocb_src,
+			       struct file *filp)
+{
+	*kiocb = (struct kiocb) {
+		.ki_filp = filp,
+		.ki_flags = kiocb_src->ki_flags,
+		.ki_hint = kiocb_src->ki_hint,
+		.ki_ioprio = kiocb_src->ki_ioprio,
+		.ki_pos = kiocb_src->ki_pos,
+	};
 }
 
 ssize_t fuse_passthrough_read_iter(struct kiocb *iocb_fuse,
@@ -266,75 +266,6 @@ err_free_file:
 	return res;
 }
 
-#include <linux/idr.h>
-
-int fuse_passthrough_open(struct fuse_dev *fud,
-			  struct fuse_passthrough_out *pto)
-{
-
-	return -EINVAL;
-
-	int res;
-	struct file *passthrough_filp;
-	struct fuse_conn *fc = fud->fc;
-	struct inode *passthrough_inode;
-	struct super_block *passthrough_sb;
-	struct fuse_passthrough *passthrough;
-
-	if (!fc->passthrough)
-		return -EPERM;
-
-	/* This field is reserved for future implementation */
-	if (pto->len != 0)
-		return -EINVAL;
-
-	passthrough_filp = fget(pto->fd);
-	if (!passthrough_filp) {
-		pr_err("FUSE: invalid file descriptor for passthrough.\n");
-		return -EBADF;
-	}
-
-	if (!passthrough_filp->f_op->read_iter ||
-	    !passthrough_filp->f_op->write_iter) {
-		pr_err("FUSE: passthrough file misses file operations.\n");
-		res = -EBADF;
-		goto err_free_file;
-	}
-
-	passthrough_inode = file_inode(passthrough_filp);
-	passthrough_sb = passthrough_inode->i_sb;
-	if (passthrough_sb->s_stack_depth >= FILESYSTEM_MAX_STACK_DEPTH) {
-		pr_err("FUSE: fs stacking depth exceeded for passthrough\n");
-		res = -EINVAL;
-		goto err_free_file;
-	}
-
-	passthrough = kmalloc(sizeof(struct fuse_passthrough), GFP_KERNEL);
-	if (!passthrough) {
-		res = -ENOMEM;
-		goto err_free_file;
-	}
-
-	passthrough->filp = passthrough_filp;
-
-	idr_preload(GFP_KERNEL);
-	spin_lock(&fc->passthrough_req_lock);
-	res = idr_alloc(&fc->passthrough_req, passthrough, 1, 0, GFP_ATOMIC);
-	spin_unlock(&fc->passthrough_req_lock);
-	idr_preload_end();
-
-	if (res > 0)
-		return res;
-
-	fuse_passthrough_release(passthrough);
-	kfree(passthrough);
-
-err_free_file:
-	fput(passthrough_filp);
-
-	return res;
-}
-
 int fuse_passthrough_setup(struct fuse_conn *fc, struct fuse_file *ff,
 			   struct fuse_open_out *openarg)
 {
@@ -359,7 +290,6 @@ int fuse_passthrough_setup(struct fuse_conn *fc, struct fuse_file *ff,
 	kfree(passthrough);
 
 	return 0;
-	return -EINVAL;
 }
 
 void fuse_passthrough_release(struct fuse_passthrough *passthrough)
