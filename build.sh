30a2ff57eb96b180e4eb62abbd40dbf34354e964
likkai <76016664+likkai@users.noreply.github.com>
2023-11-15 00:50:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ echo -e "\nKernel compiled succesfully! Zipping up...\n"
 if [ -d "$AK3_DIR" ]; then
 	cp -r $AK3_DIR AnyKernel3
 	git -C AnyKernel3 checkout lisa &> /dev/null
-elif ! git clone -q https://github.com/ghostrider-reborn/AnyKernel3 -b lisa; then
+elif ! git clone -q https://github.com/likkai/AnyKernel3 -b lisa; then
 	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
 	exit 1
 fi
