162b585cee9eab53b552cea69d6bb719800ec214
Juhyung Park <qkrwngud825@gmail.com>
2023-11-30 21:58:15 +0900

    char: add LRNG v52
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8,6 +8,8 @@
  */
 #include "sched.h"
 
+#include <linux/lrng.h>
+
 #include <linux/nospec.h>
 
 #include <linux/kcov.h>
@@ -2519,6 +2521,8 @@ static void
 ttwu_stat(struct task_struct *p, int cpu, int wake_flags)
 {
 	struct rq *rq;
+
+	add_sched_randomness(p, cpu);
 
 	if (!schedstat_enabled())
 		return;
