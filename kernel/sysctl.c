1cfef69d9f1b9ecff21ec4a74f983524a5e95837
Masahito S <firelzrd@gmail.com>
2024-03-28 00:50:30 +0900

    sched_bore: no longer switchable

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1623,7 +1623,7 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler = proc_douintvec_minmax,
-		.extra1		= SYSCTL_ZERO,
+		.extra1		= SYSCTL_ONE,
 		.extra2		= SYSCTL_ONE,
 	},
 	{
