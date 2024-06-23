df84c703cb14f469a2e7b530ccbd1faba4aaa6c3
tew404 <thammarat.tew@gmail.com>
2024-06-23 16:30:11 +0700

    Revert

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -16,7 +16,7 @@ static struct devfreq_msm_adreno_tz_data adreno_tz_data = {
 		.max = 1600,
 		.floating = true,
 	},
-	.mod_percent = 95,
+	.mod_percent = 100,
 };
 
 /**
@@ -736,13 +736,13 @@ static void pwrscale_of_ca_aware(struct kgsl_device *device)
 	struct device_node *parent = device->pdev->dev.of_node;
 	struct device_node *node, *child;
 
-	pwrscale->ctxt_aware_enable = true;
+	pwrscale->ctxt_aware_enable = 
 		of_property_read_bool(parent, "qcom,enable-ca-jump");
 
 	if (!pwrscale->ctxt_aware_enable)
 		return;
 
-	pwrscale->ctxt_aware_busy_penalty = 10000;
+	pwrscale->ctxt_aware_busy_penalty = 12000;
 	of_property_read_u32(parent, "qcom,ca-busy-penalty",
 		&pwrscale->ctxt_aware_busy_penalty);
 
