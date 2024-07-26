b625f2b61da8e607f6c2638ee71c648c815c9154
tew404 <thammarat.tew@gmail.com>
2024-07-26 17:58:30 +0700

    Update

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -5318,6 +5318,7 @@ static int binder_ioctl_get_node_debug_info(struct binder_proc *proc,
 	return 0;
 }
 
+#if 0
 static bool binder_txns_pending_ilocked(struct binder_proc *proc)
 {
 	struct rb_node *n;
@@ -5411,6 +5412,7 @@ static int binder_ioctl_get_freezer_info(
 
 	return 0;
 }
+#endif
 
 static long binder_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
