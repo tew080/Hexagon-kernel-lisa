94d7f8a64719e03b0ea90415d66d6147566ec0f9
tew404 <thammarat.tew@gmail.com>
2024-08-01 14:35:58 +0700

    Revert "sched: Implement tapered dvfs_headroom"
    This reverts commit 2338cb03315f9db29960e8ff69877bd137a02721.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -286,10 +286,8 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * frequency will be gracefully reduced with the utilization decay.
 	 */
 	util = util_cfs + cpu_util_rt(rq);
-	if (type == FREQUENCY_UTIL) {
-		util = apply_dvfs_headroom(util, cpu, true);
+	if (type == FREQUENCY_UTIL)
 		util = uclamp_rq_util_with(rq, util, p);
-	}
 
 	dl_util = cpu_util_dl(rq);
 
@@ -322,7 +320,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 *                 max
 	 */
 	util = scale_irq_capacity(util, irq, max);
-	util += type == FREQUENCY_UTIL ? apply_dvfs_headroom(irq, cpu, false) : irq;
+	util += irq;
 
 	/*
 	 * Bandwidth required by DEADLINE must always be granted while, for
@@ -335,7 +333,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * an interface. So, we only do the latter for now.
 	 */
 	if (type == FREQUENCY_UTIL)
-		util += apply_dvfs_headroom(cpu_bw_dl(rq), cpu, false);
+		util += cpu_bw_dl(rq);
 
 	return min(max, util);
 }
