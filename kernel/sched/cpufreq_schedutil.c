46b3ced2ac7fd8d5049b11047a3a6906c33a4729
tew404 <thammarat.tew@gmail.com>
2024-08-01 14:35:43 +0700

    Revert "sched: cpufreq: Apply DVFS headroom to CFS only"
    This reverts commit cc1b3d997eb05e7602a720a57e29ce163eaab369.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -285,11 +285,10 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * When there are no CFS RUNNABLE tasks, clamps are released and
 	 * frequency will be gracefully reduced with the utilization decay.
 	 */
+	util = util_cfs + cpu_util_rt(rq);
 	if (type == FREQUENCY_UTIL) {
-		util = apply_dvfs_headroom(util_cfs, cpu) + cpu_util_rt(rq);
+		util = apply_dvfs_headroom(util, cpu, true);
 		util = uclamp_rq_util_with(rq, util, p);
-	} else {
-		util = util_cfs + cpu_util_rt(rq);
 	}
 
 	dl_util = cpu_util_dl(rq);
@@ -323,7 +322,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 *                 max
 	 */
 	util = scale_irq_capacity(util, irq, max);
-	util += irq;
+	util += type == FREQUENCY_UTIL ? apply_dvfs_headroom(irq, cpu, false) : irq;
 
 	/*
 	 * Bandwidth required by DEADLINE must always be granted while, for
@@ -336,7 +335,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * an interface. So, we only do the latter for now.
 	 */
 	if (type == FREQUENCY_UTIL)
-		util += cpu_bw_dl(rq);
+		util += apply_dvfs_headroom(cpu_bw_dl(rq), cpu, false);
 
 	return min(max, util);
 }
