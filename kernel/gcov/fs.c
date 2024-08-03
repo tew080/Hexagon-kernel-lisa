91bfcdcf00420478b6c770ff28f94a314e280a98
tew404 <thammarat.tew@gmail.com>
2024-08-03 08:51:21 +0700

    Revert "BACKPORT: gcov: use kvmalloc()"
    This reverts commit bc991bab71b590832dfb019acfbd1471be5420b4.

--- kernel/gcov/fs.c
+++ kernel/gcov/fs.c
@@ -26,7 +26,7 @@
 #include <linux/slab.h>
 #include <linux/mutex.h>
 #include <linux/seq_file.h>
-#include <linux/mm.h>
+#include <linux/vmalloc.h>
 #include "gcov.h"
 
 /**
@@ -116,7 +116,7 @@ static struct gcov_iterator *gcov_iter_new(struct gcov_info *info)
 	/* Dry-run to get the actual buffer size. */
 	size = convert_to_gcda(NULL, info);
 
-	iter = kvmalloc(struct_size(iter, buffer, size), GFP_KERNEL);
+	iter = vmalloc(struct_size(iter, buffer, size));
 	if (!iter)
 		return NULL;
 
@@ -134,7 +134,7 @@ static struct gcov_iterator *gcov_iter_new(struct gcov_info *info)
  */
 static void gcov_iter_free(struct gcov_iterator *iter)
 {
-	kvfree(iter);
+	vfree(iter);
 }
 
 /**
