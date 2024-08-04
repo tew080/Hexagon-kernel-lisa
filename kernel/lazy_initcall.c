e3f3f7911fa9b292e569eaa4202bf11d128d97d6
tew404 <thammarat.tew@gmail.com>
2024-08-04 12:02:05 +0700

    Revert "lazy_initcall: fix memory-leaks on info->hdr"
    This reverts commit a593f2bb310709cb60d6dbbaa57348f59214261b.

--- kernel/lazy_initcall.c
+++ kernel/lazy_initcall.c
@@ -353,6 +353,7 @@ static noinline int __init __load_module(struct load_info *info, const char __us
 	load_modname(info->name, uargs, NORMAL);
 
 err:
+	free_copy(info);
 	return err;
 }
 
@@ -386,7 +387,6 @@ static int __ref load_module(struct load_info *info, const char __user *uargs,
 	}
 
 out:
-	free_copy(info);
 	mutex_unlock(&lazy_initcall_mutex);
 	return ret;
 }
