37b6d842cf9afe3e0a2dd6175a8bf3a0b575176e
tew404 <thammarat.tew@gmail.com>
2024-09-01 18:46:37 +0700

    Use power efficient workingqueues

--- fs/fuse/virtio_fs.c
+++ fs/fuse/virtio_fs.c
@@ -303,7 +303,7 @@ static void virtio_fs_request_dispatch_work(struct work_struct *work)
 			if (ret == -ENOMEM || ret == -ENOSPC) {
 				spin_lock(&fsvq->lock);
 				list_add_tail(&req->list, &fsvq->queued_reqs);
-				schedule_delayed_work(&fsvq->dispatch_work,
+			queue_delayed_work(system_power_efficient_wq,&fsvq->dispatch_work,
 						      msecs_to_jiffies(1));
 				spin_unlock(&fsvq->lock);
 				return;
@@ -359,7 +359,7 @@ static void virtio_fs_hiprio_dispatch_work(struct work_struct *work)
 					 ret);
 				list_add_tail(&forget->list,
 						&fsvq->queued_reqs);
-				schedule_delayed_work(&fsvq->dispatch_work,
+			queue_delayed_work(system_power_efficient_wq,&fsvq->dispatch_work,
 						msecs_to_jiffies(1));
 			} else {
 				pr_debug("virtio-fs: Could not queue FORGET: err=%d. Dropping it.\n",
@@ -796,7 +796,7 @@ __releases(fiq->lock)
 			pr_debug("virtio-fs: Could not queue FORGET: err=%d. Will try later.\n",
 				 ret);
 			list_add_tail(&forget->list, &fsvq->queued_reqs);
-			schedule_delayed_work(&fsvq->dispatch_work,
+		queue_delayed_work(system_power_efficient_wq,&fsvq->dispatch_work,
 					msecs_to_jiffies(1));
 			inc_in_flight_req(fsvq);
 		} else {
@@ -1046,7 +1046,7 @@ __releases(fiq->lock)
 			spin_lock(&fsvq->lock);
 			list_add_tail(&req->list, &fsvq->queued_reqs);
 			inc_in_flight_req(fsvq);
-			schedule_delayed_work(&fsvq->dispatch_work,
+		queue_delayed_work(system_power_efficient_wq,&fsvq->dispatch_work,
 						msecs_to_jiffies(1));
 			spin_unlock(&fsvq->lock);
 			return;
@@ -1057,7 +1057,7 @@ __releases(fiq->lock)
 		/* Can't end request in submission context. Use a worker */
 		spin_lock(&fsvq->lock);
 		list_add_tail(&req->list, &fsvq->end_reqs);
-		schedule_delayed_work(&fsvq->dispatch_work, 0);
+	queue_delayed_work(system_power_efficient_wq,&fsvq->dispatch_work, 0);
 		spin_unlock(&fsvq->lock);
 		return;
 	}
