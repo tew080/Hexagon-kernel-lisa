8dea551194426f760fc5c42abe46b54e71cc4183
tew404 <thammarat.tew@gmail.com>
2024-09-01 11:00:26 +0700

    Import touch drivers from LineageOS

--- drivers/input/touchscreen/gt9897t/goodix_brl_fwupdate.c
+++ drivers/input/touchscreen/gt9897t/goodix_brl_fwupdate.c
@@ -1,7 +1,6 @@
  /*
   * Goodix Touchscreen Driver
   * Copyright (C) 2020 - 2021 Goodix, Inc.
-  * Copyright (C) 2021 XiaoMi, Inc.
   *
   * This program is free software; you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
